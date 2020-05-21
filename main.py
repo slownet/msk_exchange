53121a6a1e3d9f882030609d70755bdef4b4c044
Maxim Bilan <bilan@europe.com>
2020-05-21 16:08:36 +0200

    Dividend list method

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ from stock import Stock
 if __name__ == "__main__":
     yandex_stock = Stock("YNDX")
     sber_stock = Stock("SBER")
+    tatn_stock = Stock("TATN")
 
     print(yandex_stock)
     print(sber_stock)
+
+    print()
+
+    date_from = "2017-05-20"
+    date_till = "2018-05-20"  # optional parameter
+
+    print(f"{tatn_stock.get_full_name()} - dividend list from {date_from} till {date_till}")
+
+    for dividend in tatn_stock.get_dividends(date_from, date_till):
+        print(f"{dividend['registryclosedate']} - {dividend['value']} ({dividend['currencyid']})")
--- stock.py
+++ stock.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 import requests
 
 
@@ -8,7 +10,18 @@ def do_request(stock_id: str = None,
                request_parameters: dict = None) -> dict:
     # create url by template
 
-    url = f"https://iss.moex.com/iss/engines/{engine}/markets/{market}/boards/{board}/securities"
+    url = f"https://iss.moex.com/iss"
+
+    if engine:
+        url += f"/engines/{engine}"
+
+    if market:
+        url += f"/markets/{market}"
+
+    if board:
+        url += f"/boards/{board}"
+
+    url += "/securities"
 
     # add stock id if needed
     if stock_id is not None:
@@ -68,6 +81,42 @@ class Stock:
         )
 
         return response_dict["SECNAME"]
+
+    def get_dividends(self, date_from: str, date_till: str = str(date.today())) -> list:
+        """
+        Requests dividend list from a specific date.
+        Every dividend is a dictionary with such keys: ['secid', 'isin', 'registryclosedate', 'value', 'currencyid']
+
+        Request exmample:
+            http://iss.moex.com/iss/securities/TATN/dividends.json?iss.meta=off&from=2015-07-15&till=2016-06-24&start=0&dividends.columns=registryclosedate
+
+        :param date_from: starting date in format: YYYY-MM-DD
+        :param date_till: optional date where default value is today (same format as date_from)
+
+        :return: dividends list
+        """
+
+        url = f"http://iss.moex.com/iss/securities/TATN/dividends.json"
+        request_parameters = {
+            "iss.meta": "off",
+            "from": date_from,
+            "till": date_till,
+        }
+        response = requests.get(url, params=request_parameters)
+        json_data = response.json()
+
+        # unwrap dividents layer
+        json_data = json_data["dividends"]
+
+        # FIXME: find how to filter data in request
+        # strings are compared lexicographically
+        all_dividends_list = [dict(zip(json_data["columns"], dividend))
+                              for dividend in json_data["data"]]
+
+        filtered_dividends_list = [dividend for dividend in all_dividends_list
+                                   if date_from <= dividend["registryclosedate"] <= date_till]
+
+        return filtered_dividends_list
 
     def __str__(self) -> str:
         return f"{self.get_id()} - {self.get_full_name()}: {self.get_price()}"
